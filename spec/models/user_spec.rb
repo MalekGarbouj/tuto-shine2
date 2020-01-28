bef51553af55b5a937df7a682c4d792fc5324aa5
MalekGarbouj <malekgarbouj40@gmail.com>
2020-01-28 16:46:24 +0100

    spec User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,17 @@
 require 'rails_helper'
+require 'support/violate_check_constraint_matcher'
 require 'pry'
+
 
 describe User do
     describe "email" do
-        user = User.new(email: "fooo@example.com",
+        user = User.new(email: "foo@example.com",
                        password: "qwertyuiop",
                        password_confirmation: "qwertyuiop")
 
         it ' absolutely prevents invalid email addresses ' do
-            expect{raise "fail!"}.to raise_error
+            expect{user.update_attribute(:email, "foo@bar.com")}
+            .to change {violate_check_constraint(:email_must_be_company_email)}
         end
    end 
 end
